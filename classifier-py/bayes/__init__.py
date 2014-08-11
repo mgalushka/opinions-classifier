17f3bfeb73bfca740d70333cd0d09f3ffcb762f4
Maxim Galushka <maxim.galushka@mail.ru>
2014-08-11 14:12:02 +0300

    initial version of classifier

--- classifier-py/bayes/__init__.py
+++ classifier-py/bayes/__init__.py
@@ -1,8 +1,9 @@
+import sys
 from nltk import NaiveBayesClassifier
 from collections import defaultdict
 
 
-def gender_features(sentence):
+def sentence_features(sentence):
     """
     :param sentence:
     :return: Extracts features from input sentence
@@ -10,7 +11,10 @@ def gender_features(sentence):
     words = sentence.split(" ")
     map = defaultdict(int)
     for w in words:
-        w = w.strip()
+        w = w.lower().strip()
+        # exclude URLs
+        if w.startswith("http"):
+            continue
         map[w] += 1
     features = {}
     for k in map.iterkeys():
@@ -24,16 +28,23 @@ feature_probdist = []
 
 train_set = []
 
-f = file("D:/projects/opinions-classifier/twitter-java/data/data.labeled.txt", "r")
-l = f.readline()
-print gender_features(l)
+f = file(sys.argv[1], "r")
+l = "x"
+while l:
+    l = f.readline()
+    s = l.find(",")
+    label = l[:s]
+    feature = sentence_features(l[s + 2:])
+    print "%s -> %s" % (label, feature)
+    train_set.append((feature, label))
 
+print(train_set)
 f.close()
 
-"""
 classifier = NaiveBayesClassifier.train(train_set)
-b = NaiveBayesClassifier(label_probdist, feature_probdist)
 
-c = b.classify(input)
-"""
+# dummy test
+print(classifier.classify(sentence_features(
+    ".@promothos Secondly, &most important, the fascist behavior THE GOVT demonstrated implementing E.#Ukraine genocide @JamieK_UNB @sn0wba111")))
+
 

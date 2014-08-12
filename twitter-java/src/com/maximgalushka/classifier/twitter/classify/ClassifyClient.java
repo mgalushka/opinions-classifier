1180913c1b5f91016ae17ce217f5f4dbde9eefb2
Maxim Galushka <maxim.galushka@mail.ru>
2014-08-12 14:27:26 +0300

    imports

--- twitter-java/src/com/maximgalushka/classifier/twitter/classify/ClassifyClient.java
+++ twitter-java/src/com/maximgalushka/classifier/twitter/classify/ClassifyClient.java
@@ -1,9 +1,6 @@
 package com.maximgalushka.classifier.twitter.classify;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.*;
 import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 

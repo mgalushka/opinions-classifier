460e02c55abb6630c2ef09366dc3065496059dfe
Maxim Galushka <mgalushka@gmail.com>
2014-08-13 18:47:34 +0300

    we are near

--- classifier-py/bayes/WordExistsFeatures.py
+++ classifier-py/bayes/WordExistsFeatures.py
@@ -6,23 +6,24 @@ import re
 
 class WordExistsFeaturesExtractor:
     def __init__(self, full_data_file):
-        self.excluded = set()
+        # manually exclude all punctuation signs
+        self.excluded = {"'", "!", ".", ",", ":", "-", "\""}
         self.preprocess(full_data_file)
 
     def clean(self, text):
         # http://stackoverflow.com/a/6883094/2075157
-        return re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text, '', flags=re.MULTILINE)
+        return re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text,
+                      flags=re.MULTILINE)
 
     def preprocess(self, full_data_file):
         labeled_file = codecs.open(full_data_file, "r", "utf-8")
         all_word_counts = defaultdict(int)
         line = "x"
         while line:
-            line = self.clean(labeled_file.readline().encode("utf-8"))
-            raw_tweet_text = line[line.find(",") + 1:-1]
+            line = labeled_file.readline().encode("utf-8")
+            raw_tweet_text = self.clean(line[line.find(",") + 2:-3]).strip()
             tokens = nltk.wordpunct_tokenize(raw_tweet_text)
             for w in tokens:
-                w = w.lower().strip()
                 if w:
                     all_word_counts[w] += 1
         labeled_file.close()

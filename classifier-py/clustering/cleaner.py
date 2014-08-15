7e4503317a4fcabd1bb206429789490145cdfffc
Maxim Galushka <mgalushka@gmail.com>
2014-08-15 19:06:19 +0300

    clustering

---
+++ classifier-py/clustering/cleaner.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import codecs
+import sys
+import features
+
+if __name__ == "__main__":
+    from_file = codecs.open(sys.argv[1], "r", "utf-8")
+    to_file = codecs.open(sys.argv[2], "wb", "utf-8")
+    f = features.StemExistsFeaturesExtractor()
+    line = "x"
+    while line:
+        line = from_file.readline().encode("utf-8")
+        raw_tweet_text = f.clean_all(line).strip()
+        if raw_tweet_text:
+            to_file.write(raw_tweet_text)
+    to_file.flush()
+    to_file.close()
+    from_file.close()

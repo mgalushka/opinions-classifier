c433ea91cf3532311a1b57aafb0c33f7b9ad6084
Maxim Galushka <mgalushka@gmail.com>
2014-08-22 16:29:28 +0300

    long-polling

--- web/js/main.js
+++ web/js/main.js
@@ -15,4 +15,16 @@ $( document ).ready(function() {
 		msnry.layout();
 	});
 	
+	
+	(function poll(){
+		setTimeout(function(){
+		  $.ajax({ url: "http://localhost:8080", success: function(data){
+			//Update your dashboard gauge
+			//salesGauge.setValue(data.value);
+
+			//Setup the next poll recursively
+			poll();
+		  }, dataType: "json"});
+		}, 30000);
+	})();
 });

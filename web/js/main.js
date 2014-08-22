180a449362a0309412fd890f0d0ff005bbeefe39
Maxim Galushka <mgalushka@gmail.com>
2014-08-22 17:19:30 +0300

    dynamic blocks adding

--- web/js/main.js
+++ web/js/main.js
@@ -15,17 +15,58 @@ $( document ).ready(function() {
 		msnry.layout();
 	});
 	
+	function getItemElement() {
+		var elem = document.createElement('div');
+		var wRand = Math.random();
+		var hRand = Math.random();
+		var widthClass = wRand > 0.92 ? 'w4' : wRand > 0.84 ? 'w3' : wRand > 0.65 ? 'w2' : '';
+		var heightClass = hRand > 0.85 ? 'h4' : hRand > 0.6 ? 'h3' : hRand > 0.35 ? 'h2' : '';
+		elem.className = 'item ' + widthClass + ' ' + heightClass;
+		return elem;
+	}
+
+	function prepend(){
+		var container = $('.masonry');
+		//var msnry = $('.masonry').data('masonry');
+		// create new item elements
+		var elems = [];
+		var fragment = document.createDocumentFragment();
+		for ( var i = 0; i < 3; i++ ) {
+		  var elem = getItemElement();
+		  fragment.appendChild( elem );
+		  elems.push( elem );
+		}
+		// prepend elements to container
+		$('.item').first().before(fragment);
+		//alert(fragment.toHtml());
+		// add and lay out newly prepended elements
+		msnry.prepended( elems );
+	    //msnry.layout();
+		for(var i = 0; i < 3; i++){
+			$(elems[i]).removeClass('item').addClass('item');
+		}
+		console.log( "prepended" );
+	}
+	
+	function poll(){
+		setTimeout(function(){
+		  $.ajax({ url: "http://localhost", 
+			success: function(data){
+				prepend();
+				poll();
+			}, 
+			error: function(data){
+				prepend();
+				poll();
+			}, 
+			dataType: "json"});
+		}, 1000);
+	};
+	
+	poll();
+	
 });
 
-(function poll(){
-	setTimeout(function(){
-	  $.ajax({ url: "http://localhost", 
-		success: function(data){
-			poll();
-		}, 
-		error: function(data){
-			poll();
-		}, 
-		dataType: "json"});
-	}, 10000);
-})();
+
+
+

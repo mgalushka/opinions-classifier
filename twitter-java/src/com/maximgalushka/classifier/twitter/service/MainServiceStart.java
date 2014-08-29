be8abd8bae31577f98acd91581c9681c4af4b804
Maxim Galushka <maxim.galushka@mail.ru>
2014-08-29 17:17:55 +0300

    simpleframework really working service

--- twitter-java/src/com/maximgalushka/classifier/twitter/service/MainServiceStart.java
+++ twitter-java/src/com/maximgalushka/classifier/twitter/service/MainServiceStart.java
@@ -1,20 +1,52 @@
 package com.maximgalushka.classifier.twitter.service;
 
-import org.apache.log4j.Logger;
-import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
-import java.net.URI;
+import org.apache.log4j.Logger;
+import org.simpleframework.http.Request;
+import org.simpleframework.http.Response;
+import org.simpleframework.http.core.Container;
+import org.simpleframework.http.core.ContainerServer;
+import org.simpleframework.transport.Server;
+import org.simpleframework.transport.connect.Connection;
+import org.simpleframework.transport.connect.SocketConnection;
 
 /**
  * @since 8/29/2014.
  */
-public class MainServiceStart {
+public class MainServiceStart implements Container {
 
     public static final Logger log = Logger.getLogger(MainServiceStart.class);
-    public static final String URL = "http://localhost:8090/topics";
 
-    public static void main(String[] args) {
-        JdkHttpServerFactory.createHttpServer(URI.create(URL), new TwitterTopicsService());
-        log.debug(String.format("Twitter topics service started on: [%s]", URL));
+    public void headers(Request request, Response response) {
+        long time = System.currentTimeMillis();
+        response.setValue("Content-Type", "application/json");
+        response.setValue("Server", "Tweets Clustering Classifier");
+        response.setDate("Date", time);
+        response.setDate("Last-Modified", time);
+        response.setValue("Access-Control-Allow-Origin", "*");
+    }
+
+    public void handle(Request request, Response response) {
+        try {
+            headers(request, response);
+            PrintStream body = response.getPrintStream();
+            body.println("{h:1}");
+            body.close();
+            log.debug("Response sent");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] list) throws Exception {
+        Container container = new MainServiceStart();
+        Server server = new ContainerServer(container);
+        Connection connection = new SocketConnection(server);
+        SocketAddress address = new InetSocketAddress(8090);
+        connection.connect(address);
+        log.debug("Server started");
     }
 }

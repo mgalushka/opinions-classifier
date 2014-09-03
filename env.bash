7fa94d76b2988f03c481edf67fe2bf2376fc93a0
Maxim Galushka <maxim.galushka@mail.ru>
2014-09-03 19:24:02 +0300

    ehnv config

---
+++ env.bash
@@ -0,0 +1,11 @@
+# public URL
+# http://ec2-54-68-39-246.us-west-2.compute.amazonaws.com/opinions-classifier/web/
+
+export JAVA_HOME=/opt/jdk/jdk1.7.0_67
+export M2_HOME=~/apache-maven-3.2.3
+export PATH=${JAVA_HOME}/bin:${PATH}:${M2_HOME}/bin
+
+
+# how to download java from oracle site
+# wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz
+

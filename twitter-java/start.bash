5b7525d2f50f1b0687be09db105be72ea382d597
Maxim Galushka <maxim.galushka@mail.ru>
2014-09-11 17:08:57 +0300

    start

--- twitter-java/start.bash
+++ twitter-java/start.bash
@@ -1,3 +1,3 @@
 #!/bin/bash   
 
-java -cp ./target/dependency/*:./target/opinions-tweets-classifier-1.0.jar com.maximgalushka.classifier.twitter.service.MainServiceStart
+nohup java -cp ./target/dependency/*:./target/opinions-tweets-classifier-1.0.jar com.maximgalushka.classifier.twitter.service.MainServiceStart > out.log &

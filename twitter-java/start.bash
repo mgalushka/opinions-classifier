d03f5cda1b56078bc6c3f64ab0aef534e5fff242
Maxim Galushka <maxim.galushka@mail.ru>
2014-09-11 17:57:00 +0300

    popd

--- twitter-java/start.bash
+++ twitter-java/start.bash
@@ -1,3 +1,6 @@
 #!/bin/bash   
 
-nohup java -cp ./target/dependency/*:./target/opinions-tweets-classifier-1.0.jar com.maximgalushka.classifier.twitter.service.MainServiceStart > out.log &> /dev/null
+nohup java -cp ./target/dependency/*:./target/opinions-tweets-classifier-1.0.jar com.maximgalushka.classifier.twitter.service.MainServiceStart > out.log &
+STAT=$?
+popd
+sleep 1

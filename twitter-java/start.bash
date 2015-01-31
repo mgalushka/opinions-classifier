adf92a7fccecf8b64e0986173bb4b332aac31a72
Maxim Galushka <maxim.galushka@mail.ru>
2015-01-31 13:14:28 +0000

    jmx monitoring

--- twitter-java/start.bash
+++ twitter-java/start.bash
@@ -1,4 +1,9 @@
 #!/bin/bash   
 
-nohup java -cp ./target/dependency/*:./target/opinions-tweets-classifier-1.0.jar -Xms512m -Xmx512m com.maximgalushka.classifier.twitter.service.MainServiceStart > out.log 2>&1 &
+CLASSPATH="./target/dependency/*:./target/opinions-tweets-classifier-1.0.jar"
+JAVA_MEM="-Xms768m -Xmx768m"
+JAVA_JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+JAVA_GC="-verbose:gc"
+
+nohup java -cp ${CLASSPATH} ${JAVA_MEM} ${JAVA_GC} ${JAVA_JMX} com.maximgalushka.classifier.twitter.service.MainServiceStart > out.log 2>&1 &
 echo 0

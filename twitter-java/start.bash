240697723d09396a6825af33e0ff90d032e2b4be
Maxim Galushka <maxim.galushka@mail.ru>
2015-02-12 00:16:25 +0000

    jstatd

--- twitter-java/start.bash
+++ twitter-java/start.bash
@@ -5,6 +5,8 @@ JAVA_MEM="-Xms1G -Xmx1G"
 JAVA_JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 JAVA_GC="-verbose:gc"
 
+
+# jstatd -J-Djava.security.policy=/home/ec2-user/jstatd.all -p 1234 -J-Djava.rmi.server.logCalls=true
 # nohup jstatd -J-Djava.security.policy=/home/ec2-user/jstatd.all -p 8089 &
 
 nohup java -cp ${CLASSPATH} ${JAVA_MEM} ${JAVA_GC} ${JAVA_JMX} com.maximgalushka.classifier.twitter.service.MainServiceStart > out.log 2>&1 &

632ea0c06ac148bc91f8545f255983e63d575941
Maxim Galushka <maxim.galushka@mail.ru>
2015-04-08 20:49:50 +0100

    2 hours

--- twitter-java/src/com/maximgalushka/classifier/clustering/ClusteringPipeline.java
+++ twitter-java/src/com/maximgalushka/classifier/clustering/ClusteringPipeline.java
@@ -81,7 +81,7 @@ public class ClusteringPipeline {
     this.twitterClient = twitterClient;
   }
 
-  private static final int LATEST_HOURS = 4;
+  private static final int LATEST_HOURS = 2;
 
   /**
    * Business method:

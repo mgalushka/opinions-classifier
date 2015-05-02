bed92a8a56f9c6e799af1b91596f1d4871f4c762
Maxim Galushka <mgalushka@fb.com>
2015-05-01 20:44:22 -0700

    small

--- web/tools/tweets.php
+++ web/tools/tweets.php
@@ -152,7 +152,7 @@ $count_row= DB::queryFirstRow("
 <!-- Modal -->
 <div class="modal modal-sm" id="edit-tweet-modal" tabindex="-1" role="dialog" aria-labelledby="edit-tweet-modal-label"
      aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-sm">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span

53be018f6aa504f19a1aa3b0dfc842b3a0c5bb45
Maxim Galushka <maxim.galushka@mail.ru>
2015-05-17 12:12:19 +0100

    path

--- classifier-py/decision_trees/good_tweet_classifier.py
+++ classifier-py/decision_trees/good_tweet_classifier.py
@@ -5,7 +5,10 @@ from nltk.corpus.reader.plaintext import (
 )
 
 decisions = CategorizedPlaintextCorpusReader(
-    '~/projects/opinions-classifier/classifier-py/data/tweets_publish_choice',
+    '{0}/projects/opinions-classifier/'
+    'classifier-py/data/tweets_publish_choice'.format(
+        os.getenv("HOME")
+    ),
     r'.*\.txt',
     cat_pattern=r'(\w+)/*'
 )
@@ -14,12 +17,14 @@ word_features = list(all_words)[:2000]
 
 print(word_features)
 
+
 def text_features(txt):
     document_words = set(txt)
     features = {}
     for word in word_features:
         features['contains({})'.format(word)] = (word in document_words)
     return features
+
 
 def prepare_labeled_data():
     pass

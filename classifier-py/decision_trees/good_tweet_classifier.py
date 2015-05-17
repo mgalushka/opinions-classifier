561fe0c33d3cc77970595bc38f8221eb63b80e98
Maxim Galushka <maxim.galushka@mail.ru>
2015-05-17 13:05:02 +0100

    documents

--- classifier-py/decision_trees/good_tweet_classifier.py
+++ classifier-py/decision_trees/good_tweet_classifier.py
@@ -15,6 +15,7 @@ size = 1979
 all_words = nltk.FreqDist(w.lower() for w in decisions.words())
 word_features = list(all_words)[:2000]
 
+
 def text_features(txt):
     document_words = set(txt)
     features = {}
@@ -22,7 +23,11 @@ def text_features(txt):
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
-featuresets = [(text_features(d), c) for (d, c) in decisions]
+
+documents = [(list(decisions.words(fileid)), category)
+             for category in decisions.categories()
+             for fileid in decisions.fileids(category)]
+featuresets = [(text_features(d), c) for (d, c) in documents]
 
 train_set, test_set = featuresets[size / 2:], featuresets[:size / 2]
 classifier = nltk.DecisionTreeClassifier.train(train_set)

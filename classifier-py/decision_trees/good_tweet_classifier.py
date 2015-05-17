b0c2fdd37fd0fb04088ae0689e52bb9e37ff489b
Maxim Galushka <max_from_sumy@mail.ru>
2015-05-17 20:22:24 +0000

    200

--- classifier-py/decision_trees/good_tweet_classifier.py
+++ classifier-py/decision_trees/good_tweet_classifier.py
@@ -12,7 +12,7 @@ decisions = LazyCorpusLoader(
     cat_pattern=r'(\w+)/*',
     encoding='utf8',
 )
-size = 1979
+
 all_words = nltk.FreqDist(w.lower() for w in decisions.words() if not w.lower() in stopwords.words('english'))
 word_features = list(all_words)[:50]
 nltk.FreqDist.pprint(all_words, 50)
@@ -43,6 +43,8 @@ chosen_docs_200 = pos_docs[:100] + neg_docs[:100]
 random.shuffle(chosen_docs_200)
 
 featuresets = [(text_features(d), c) for (d, c) in chosen_docs_200]
+
+size = 200
 
 train_set, test_set = featuresets[size / 2:], featuresets[:size / 2]
 classifier = nltk.DecisionTreeClassifier.train(train_set)

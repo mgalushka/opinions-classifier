53f183f804cb21e9d5b7bbb80f9cea520143816f
Maxim Galushka <maxim.galushka@mail.ru>
2015-06-03 21:48:50 +0100

    simple

--- web/landing/subscribe.php
+++ web/landing/subscribe.php
@@ -2,10 +2,10 @@
 header('Content-type: text/html; charset=utf-8');
 include '../tools/db.php';
 
-$link = connect();
+$email = htmlspecialchars($_POST["email"]);
 
-mysqli_free_result($result_average);
-mysqli_free_result($result);
-mysqli_close($link);
+DB::insert('subscribe', array(
+  'email' => $email
+));
 ;
 ?>

7465a1559fac8fbb3b96a1abbfcc4172847fb4c3
Maxim Galushka <maxim.galushka@mail.ru>
2015-07-01 21:59:51 +0100

    finalized service

--- text-retrieval/retrieval/retrieval_service.py
+++ text-retrieval/retrieval/retrieval_service.py
@@ -4,16 +4,16 @@ from readability.readability import Document
 
 
 urls = (
-    '/retrieve(.*)', 'retrieve'
+    '/(.*)', 'retrieve'
 )
 app = web.application(urls, globals())
 
 class retrieve:
-    def GET(self, url):
+    def GET(self, request):
+        url = web.input()['text']
         print("Retrieving text for {0}".format(url))
         html = urllib.urlopen(url).read()
         return Document(html).summary()
-
 
 if __name__ == "__main__":
     app.run()

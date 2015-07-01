d60404bb6f1184fa274f479f4438b2873266117f
Maxim Galushka <maxim.galushka@mail.ru>
2015-07-01 23:14:01 +0100

    integrated article retrieval

--- web/tools/footer.php
+++ web/tools/footer.php
@@ -4,5 +4,6 @@
 <script src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/ui.js"></script>
 <script type="text/javascript" src="js/program.js"></script>
+<script type="text/javascript" src="js/retrieval.js"></script>
 </body>
 </html>

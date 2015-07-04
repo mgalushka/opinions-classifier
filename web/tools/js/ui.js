379b5684620f10fdadf16d12f97c6c68304daae7
Maxim Galushka <maxim.galushka@mail.ru>
2015-07-05 00:15:42 +0100

    to 4 sec - fade out time

--- web/tools/js/ui.js
+++ web/tools/js/ui.js
@@ -3,15 +3,16 @@ var tt = tt || {};
 tt.core = function () {
 
     var API = "http://warua.org:8092/easy";
+    var QUICK_UPDATE_FADE_OUT = 4000;
 
     var showStatus = function (data, status) {
         console.log("Status: " + status);
         console.log("Status data: " + data);
         if (status === 'success'){
-            $('#status').text('success').show().fadeOut(2000);
+            $('#status').text('success').show().fadeOut(QUICK_UPDATE_FADE_OUT);
         }
         else{
-            $('#status').text('failed').show().fadeOut(2000);
+            $('#status').text('failed').show().fadeOut(QUICK_UPDATE_FADE_OUT);
         }
     };
 

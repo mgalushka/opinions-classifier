e0216295ad2fd059211a7a19b5e20b199165edba
Maxim Galushka <maxim.galushka@mail.ru>
2015-07-05 00:00:31 +0100

    schema

--- twitter-java/db/tweets_all.sql
+++ twitter-java/db/tweets_all.sql
@@ -3,12 +3,13 @@ CREATE TABLE `tweets_all` (
 	`content_json` TEXT NULL,
 	`tweet_cleaned` TEXT NULL,
 	`features` TEXT NULL,
-	`classified` VARCHAR(100) NULL,
+	`classified` VARCHAR(100) NULL DEFAULT NULL,
 	`excluded` TINYINT(4) NOT NULL DEFAULT '0',
 	`excluded_reason` VARCHAR(256) NULL DEFAULT '0',
 	`created_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`),
-	INDEX `created_timestamp_index` (`created_timestamp`)
+	INDEX `created_timestamp_index` (`created_timestamp`),
+	INDEX `classified` (`classified`) USING HASH
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB

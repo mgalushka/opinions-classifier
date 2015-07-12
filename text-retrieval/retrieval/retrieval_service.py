42cb5cec0623ec4b3770728a9d223f4a0e2a36ff
Maxim Galushka <maxim.galushka@mail.ru>
2015-07-12 15:42:54 +0100

    better header

--- text-retrieval/retrieval/retrieval_service.py
+++ text-retrieval/retrieval/retrieval_service.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import urllib
+import urllib2
 import web
 from readability.readability import Document
 
@@ -10,13 +10,22 @@ urls = (
 )
 app = web.application(urls, globals())
 
+
 class retrieve:
     def GET(self, request):
         web.header('Access-Control-Allow-Origin', '*')
         url = web.input()['text']
         print("Retrieving text for {0}".format(url))
-        html = urllib.urlopen(url).read().decode('utf-8')
+        request = urllib2.Request(url)
+        request.add_header(
+            'User-Agent',
+            'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'
+        )
+
+        opener = urllib2.build_opener()
+        html = opener.open(request).read().decode('utf-8', 'ignore')
         return Document(html).summary()
+
 
 if __name__ == "__main__":
     app.run()

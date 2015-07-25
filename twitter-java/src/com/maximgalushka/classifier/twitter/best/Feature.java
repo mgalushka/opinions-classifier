973e9ef8d0594e2d681d024ccc2c6b15c4dec043
Maxim Galushka <maxim.galushka@mail.ru>
2015-07-25 21:07:27 +0100

    documentation

--- twitter-java/src/com/maximgalushka/classifier/twitter/best/Feature.java
+++ twitter-java/src/com/maximgalushka/classifier/twitter/best/Feature.java
@@ -1,7 +1,11 @@
 package com.maximgalushka.classifier.twitter.best;
 
 /**
- *
+ * Feature interface.
+ * All features used to extract best tweet in cluster should extend it.
+ * <p>
+ * Feature#exclude method says if the whole tweet should be excluded completely
+ * based on feature value and used for filtering bad quality clusters.
  */
 public interface Feature<T, I> {
 
@@ -18,7 +22,8 @@ public interface Feature<T, I> {
   double metric(T feature);
 
   /**
-   * @return true is based on current metric tweet should be excluded or banned
+   * @return true if based on current metric tweet should be excluded from
+   * displaying or banned
    */
   boolean exclude(T feature);
 }
--- twitter-java/src/com/maximgalushka/classifier/twitter/best/features/RelativeClusterSize.java
+++ twitter-java/src/com/maximgalushka/classifier/twitter/best/features/RelativeClusterSize.java
@@ -2,10 +2,12 @@ package com.maximgalushka.classifier.twitter.best.features;
 
 /**
  * Relative cluster size.
- * Most significant for us - clusters with average sizes - not too small and not too big
+ * Most significant for us - clusters with average sizes - not too small and
+ * not too big
+ * <p>
  * TODO: re-think
- * We need to make more experiments with different clustering algorithms and determine if cluster
- * size is actually a good signal for us.
+ * We need to make more experiments with different clustering algorithms and
+ * determine if cluster size is actually a good signal for us.
  */
 @Deprecated
 public class RelativeClusterSize {

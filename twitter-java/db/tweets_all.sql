c67c6835860690048a07b784a44ba5761666a547
Maxim Galushka <maxim.galushka@mail.ru>
2015-07-29 22:25:05 +0100

    LONGTEXT

--- twitter-java/db/tweets_all.sql
+++ twitter-java/db/tweets_all.sql
@@ -9,7 +9,7 @@ CREATE TABLE `tweets_all` (
 	`excluded_reason` VARCHAR(256) NULL DEFAULT '0',
 	`label` VARCHAR(50) NULL DEFAULT NULL,
 	`article_extracted` TINYINT(4) NULL DEFAULT NULL,
-	`article` TEXT NULL,
+	`article` LONGTEXT NULL,
 	`created_timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`),
 	INDEX `created_timestamp_index` (`created_timestamp`),

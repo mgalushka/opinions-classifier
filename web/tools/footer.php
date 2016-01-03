1c8400636ca1970d6c273e18c188eb24355cac3f
Maxim Galushka <maxim.galushka@mail.ru>
2016-01-03 21:42:25 +0000

    mixed content

--- web/tools/footer.php
+++ web/tools/footer.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<script src="http://code.jquery.com/jquery.js"></script>
+<script src="https://code.jquery.com/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/ui.js"></script>
 <script type="text/javascript" src="js/program.js"></script>

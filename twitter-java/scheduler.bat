05d79a1e1ba399bafe5b38e0d920e891c99ca2be
Maxim Galushka <maxim.galushka@mail.ru>
2016-07-30 11:50:03 +0100

    windows scheduler start script

--- twitter-java/scheduler.bat
+++ twitter-java/scheduler.bat
@@ -1,7 +1,5 @@
-set CLASSPATH=./target/dependency/*;./target/opinions-tweets-classifier-1.0.jar
-set JAVA_MEM="-Xms128M -Xmx256M"
-set JAVA_JMX="" #"-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-set JAVA_GC="-verbose:gc"
+@echo off
 
+set CLASSPATH=./target/dependency/*;./target/opinions-tweets-classifier-1.0.jar
 java -cp %CLASSPATH% com.maximgalushka.classifier.twitter.service.TweetPublishScheduler
 
